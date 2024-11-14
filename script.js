058017e0255407f78d706cb0d6edeb39b4eab116
Grace <gracek2705@tuckahoeschools.org>
2024-11-14 13:25:56 +0000

    init commit

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+let questionList = ['Who won the 2022 NBA championship?', 'What is the tallest mountain in the world?', 'What is the largest desert in the world?'];
+let answerList = ['Golden State Warriors', 'Mount Everest', 'Antarctica'];
 let score = 0;
-let isCorrect = true;
+let questionIndex = 0;
+
+const question = document.querySelector('.question-container');
+const button = document.querySelector('.submit-button');
+const input = document.querySelector('#input-field');
+let scoreContainer = document.querySelector('.score-container');
+
+question.textContent = questionList[questionIndex];
+
+let submitFunction = () =>{
+    button.addEventListener('click', () =>{
+        if(input.value === answerList[questionIndex]){
+            questionIndex += 1;
+            score += 1;
+            question.textContent = questionList[questionIndex];
+            scoreContainer.textContent = "Score: " + score;
+        }else{
+            score -= 1;
+            scoreContainer.textContent = "Score: " + score;
+        }
+    
+        endGame();
+    });
+}
+
+let endGame = () =>{
+    if(questionIndex > 2){
+        scoreContainer.textContent = "Final Score: " + score;
+        question.textContent = "Congratulations, play again!";
+        input.style.display = "none";
+        button.style.display = "none";
+    }
+}
+
+submitFunction();
+
+
+
+
+
+
+/*let isCorrect = true;
 let answerList = ['1', '2', '3'];
 const submitButton = document.querySelector('#submit-button');
-const inputContainer = document.querySelector('.input-container');
-const question = document.querySelector('.question');
+const inputContainer = document.querySelector('.input-container');*/
 
 /*submitButton.addEventListener('click', () => {
     if (isCorrect) {
@@ -15,7 +57,6 @@ const question = document.querySelector('.question');
     isCorrect = !isCorrect;
 });*/
 
-submitButton.addEventListener('click', () => {
+/*submitButton.addEventListener('click', () => {
     console.log("hello");
-});
-
+});*/
